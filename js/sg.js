af40cf27f16f24490c32113c450fecd1b353020b
szyth <syedzia.pro@gmail.com>
2020-09-21 13:30:50 +0530

    What we do changed, Testimonials added

--- js/sg.js
+++ js/sg.js
@@ -1,9 +1,9 @@
 //owl carousel
-var owl = $(".owl-carousel");
-owl.owlCarousel({
+$(".owl-one").owlCarousel({
   items: 4,
   loop: true,
   margin: 0,
+  dots: false,
   autoplay: true,
   autoplayTimeout: 3500,
   responsive: {
@@ -21,10 +21,40 @@ owl.owlCarousel({
     },
   },
 });
-$(".play").on("click", function () {
-  owl.trigger("play.owl.autoplay", [1000]);
-});
-$(".stop").on("click", function () {
-  owl.trigger("stop.owl.autoplay");
-});
 
+$(".owl-two").owlCarousel({
+  loop: true,
+  autoplay: true,
+  dots: false,
+  autoplayTimeout: 1000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 2.3,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
+$(".owl-three").owlCarousel({
+  loop: true,
+  autoplay: true,
+  dots: false,
+  autoplayTimeout: 1000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
